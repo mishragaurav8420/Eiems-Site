98c9602f2aad001697bf180ac0a5d0bd03c9bdbf
mishragaurav8420 <mishragaurav8420@gmail.com>
2024-12-26 14:21:13 +0530

    login_api

--- backend/eiems_backend/login/service/login_service.py
+++ backend/eiems_backend/login/service/login_service.py
@@ -8,7 +8,6 @@ from rest_framework.authtoken.models import Token
 
 
 class signUpApi:
-
     def user_signin_service(request):
         username = request.data.get('username')
         password = request.data.get('password')
